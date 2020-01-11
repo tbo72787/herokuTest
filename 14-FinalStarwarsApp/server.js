16be66b0214675a3f71e113b6f2fa28446bba969
Taylor Zettel <tzettel@gmail.com>
2020-01-11 09:35:50 -0500

    class test

--- 14-FinalStarwarsApp/server.js
+++ 14-FinalStarwarsApp/server.js
@@ -5,7 +5,7 @@ var path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
+var app = process.env.PORT || 3000;
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
